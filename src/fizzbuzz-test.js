ea6d02f16402c148db7cd91b1ff16149869d4e58
cgulli <caitlin.gulliford@gmail.com>
2020-10-25 10:44:45 +0000

    implemented fizz but it dont work :( [KC]

--- src/fizzbuzz-test.js
+++ src/fizzbuzz-test.js
@@ -1,5 +1,7 @@
+/*jshint esversion: 6 */
 describe('FizzBuzz Tests', function(){
   it('should return fizz', function(){
-    expect(fizzbuzz(3)).toEqual('fizz');
+    const result = '1, 2, fizz';
+    expect(fizzbuzz(3)).toEqual(result);
   });
 });
